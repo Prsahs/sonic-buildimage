a0953569c25556e85ce47044a17028001870d874
Prsahs <run241610@gmail.com>
2026-01-13 13:19:21 +0000

    commit complie issue

--- platform/centec/docker-syncd-centec.mk
+++ platform/centec/docker-syncd-centec.mk
@@ -1,7 +1,8 @@
 # docker image for centec syncd
 
+#DOCKER_SYNCD_CENTEC = docker-syncd-centec.gz
 DOCKER_SYNCD_PLATFORM_CODE = centec
-include $(PLATFORM_PATH)/../template/docker-syncd-bullseye.mk
+include $(PLATFORM_PATH)/../template/docker-syncd-bookworm.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
 
